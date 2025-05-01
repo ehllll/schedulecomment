d498af09289d70c757b2f2a6dca42f8d54eb1e37
ehllll <leeeunhye0510@naver.com>
2025-05-02 01:32:44 +0900

    Initial commit

---
+++ src/main/java/com/example/schedulecomment/SchedulecommentApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulecomment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulecommentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulecommentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/schedulecomment/SchedulecommentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulecomment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulecommentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
